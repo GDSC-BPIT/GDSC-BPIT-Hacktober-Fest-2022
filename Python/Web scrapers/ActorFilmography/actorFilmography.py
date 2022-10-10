81a3524c2cd26e8d3a44d935a7562ac70ea32e9b
ij007 <ishantjindal30@gmail.com>
2022-10-10 22:13:01 +0530

    Made changes

--- Python/Web scrapers/ActorFilmography/actorFilmography.py
+++ Python/Web scrapers/ActorFilmography/actorFilmography.py
@@ -3,48 +3,54 @@ Take the name of the actor as input and the script returns all the moves/tv show
 '''
 
 from selenium import webdriver
+import pandas as pd
 from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
 
-name = input("Please enter the actor name: ")
-name = '+'.join(name.split())
+try:
 
-#Initiating the driver
-driver = webdriver.Firefox(executable_path = r"")
+    name = input("Please enter the actor name: ")
+    name = '+'.join(name.split())
 
-#Getting the search query
-driver.get(f"https://www.imdb.com/find?q={name}&ref_=nv_sr_sm")
+    #Initiating the driver
+    driver = webdriver.Firefox(executable_path = r"")
 
-# Navigating through search results and clicking appropriate links
-element = driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/table/tbody/tr[1]/td[2]/a').click()
-element = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div[3]/div[2]/div[3]/div[1]/div[2]/ul/li[1]/span/a').click()
+    #Getting the search query
+    driver.get(f"https://www.imdb.com/find?q={name}&ref_=nv_sr_sm")
 
-#Saving the current URL and closing the window
-URL = driver.current_url
-driver.close()
+    # Navigating through search results and clicking appropriate links
+    element = driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/table/tbody/tr[1]/td[2]/a').click()
+    element = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div[3]/div[2]/div[3]/div[1]/div[2]/ul/li[1]/span/a').click()
 
-#Getting and processing the HTML page
-r = requests.get(URL)
-soup = BeautifulSoup(r.content, 'html5lib')
+    #Saving the current URL and closing the window
+    URL = driver.current_url
+    driver.close()
 
-#Finding all films section
-table = soup.find('div', attrs = {'class':'filmo-category-section'}) 
+    #Getting and processing the HTML page
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html5lib')
+
+    #Finding all films section
+    table = soup.find('div', attrs = {'class':'filmo-category-section'}) 
 
 
-movies = {}
+    movies = {}
 
-#Filtering films from films sections with year
-for row in table.findAll('div',
-                         attrs = {'class':['filmo-row odd', 'filmo-row even']}):
-    
-    try:
-        movies[row.span.text.strip()].append(row.b.text)
-    except KeyError:
-        movies[row.span.text.strip()] = [row.b.text]
-    
+    #Filtering films from films sections with year
+    for row in table.findAll('div',
+                            attrs = {'class':['filmo-row odd', 'filmo-row even']}):
+        
+        try:
+            movies[row.span.text.strip()].append(row.b.text)
+        except KeyError:
+            movies[row.span.text.strip()] = [row.b.text]
+        
 
-#Printing fils in descending order by release date
-for key, values in movies.items():
-    for movie in values:
-        print(f"movie name: {movie} year: {key}")
+    #Printing fils in descending order by release date
+    for key, values in movies.items():
+        for movie in values:
+            print(f"movie name: {movie} year: {key}")
+
+except:
+    print('Actor name not found')
