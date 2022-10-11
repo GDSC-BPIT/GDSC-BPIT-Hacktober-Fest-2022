0559dd3b5d40194c5bc0673041adcebdedd5f469
Jatin Parashar <parashar18jatin@gmail.com>
2022-10-11 18:30:52 +0530

    Add Note Keeper using React.js

---
+++ Web Development/Frontend/Note Keeper App/src/components/CreateArea.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}><AddIcon></AddIcon></button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
