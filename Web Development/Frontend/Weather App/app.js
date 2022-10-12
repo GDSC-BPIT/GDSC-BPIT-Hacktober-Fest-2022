4ccf7922e3c7453a9d02e64af63491baeb61f6ff
Rakesh Kumar <krakesh7788@gmail.com>
2022-10-12 18:32:15 +0530

    krakesh7788@gmail.com implemented jQuery, modified UI, added pictorial view

--- Web Development/Frontend/Weather App/app.js
+++ Web Development/Frontend/Weather App/app.js
@@ -1,38 +1,76 @@
 const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
-const form = document.querySelector("form")
-const search = document.querySelector("#search")
-const weather = document.querySelector("#weather")
-    // const API = `https://api.openweathermap.org/data/2.5/weather?
-    // q=${city}&appid=${API_KEY}&units=metric`
-    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-const getWeather = async(city) => {
-    weather.innerHTML = `<h2> Loading... <h2>`
+const form = $("form")
+const search = $("#search")
+
+async function getWeather(city){
+    $("h4").html("Loading...")
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url);
     const data = await response.json()
-    return showWeather(data)
+    showWeather(data)
+    time(city)
 }
 
-const showWeather = (data) => {
+function showWeather(data){
     if (data.cod == "404") {
-        weather.innerHTML = `<h2> City Not Found <h2>`
+        $("h2").html("City Not Found")
         return;
     }
-    weather.innerHTML = `
-        <div>
-            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-        </div>
-        <div>
-            <h2>${data.main.temp} ℃</h2>
-            <h4> ${data.weather[0].main} </h4>
-        </div>
-    `
+    
+    const data2 = {
+        temp : Math.round(data.main.temp),
+        desc : data.weather[0].main,
+        icon : data.weather[0].icon
+    }
+
+    $("img.icon").attr("src", "https://openweathermap.org/img/wn/"+data2.icon+"@2x.png")
+    $("h2").html(data2.temp+" ℃")
+    $("h4").html(data2.desc)
+    showImage(data2.desc);
 }
 
-form.addEventListener(
-    "submit",
-    function(event) {
-        getWeather(search.value)
-        event.preventDefault();
+function showImage(keyword){
+    switch (keyword) {
+        case "Fog":
+            $(".backimage").attr("src", "images/fog.png");
+            break;
+        case "Clouds":
+            $(".backimage").attr("src", "images/cloudy.webp");
+            break;
+        case "Fair":
+            $(".backimage").attr("src", "images/fair.webp");
+            break;        
+        case "Sunny"||"Clear":
+            $(".backimage").attr("src", "images/sunny.webp");
+            break;
+        case "Rain"||"Drizzle":
+            $(".backimage").attr("src", "images/rain.webp");
+            break;
+        case "Mist":
+            $(".backimage").attr("src", "images/mist.jpg");
+            break;
+        case "Thunderstorm":
+            $(".backimage").attr("src", "images/thunder.webp");
+            break;
+        case "Haze":
+            $(".backimage").attr("src", "images/haze.webp");
+            break;
+        default:
+            break;
     }
-)
+}
+function time(city){
+    cityName = (city.charAt(0).toUpperCase() + city.slice(1)).bold();
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes();
+    var nameWithTime = cityName+","+ " As of "+time+" IST";
+    $("#time").addClass("time");
+    $(".time p").html(nameWithTime);
+
+}
+
+form.submit(function(event){
+        getWeather(search.val())
+        event.preventDefault();
+});
+    
